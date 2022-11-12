e745979d514f60d2be332369ff938167b023de6a
Min Kookie <min03041999@gmail.com>
2022-11-12 15:15:01 +0700

    feat(admin-login): finished a login feature of administrator

--- src/layout/Admin/index.js
+++ src/layout/Admin/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import AdminLogin from "../../components/AdminLogin";
 
 const Admin = () => {
-  return <div>Admin</div>;
+  return (
+    <div>
+      <AdminLogin />
+    </div>
+  );
 };
 
 export default Admin;
