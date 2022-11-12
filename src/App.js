87534e15532f37f2cdc3e2a839752d494c413b3f
Min Kookie <min03041999@gmail.com>
2022-11-12 09:19:11 +0700

    feat(source): build a project structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import "./App.css";
+import React, { Fragment } from "react";
 
-import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import Admin from "./layout/Admin";
+import Client from "./layout/Client";
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Fragment>
+      <Switch>
+        <Route path="/admin" component={Admin} />
+        <Route path="/" component={Client} />
+      </Switch>
+    </Fragment>
+  );
 }
 
 export default App;
